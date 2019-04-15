409061aeb4e4c9e9768bfcf98ee0b7a093e6d911
abusayedrubel <abusayedrubel0@gmail.com>
2019-04-16 01:05:39 +0600

    age calculate with a constructor function

--- app.js
+++ app.js
@@ -2,7 +2,12 @@
 function Person(name, dob){
   this.name = name;
   this.birthday = new Date(dob);
+  this.calculateAge = function(){
+    const difference = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(difference);
+    return Math.abs(ageDate.getUTCFullYear()-1970);
+  }
 }
 
 const birth = new Person('sayed', '9-22-1995');
-console.log(birth);
+console.log(birth.calculateAge());
