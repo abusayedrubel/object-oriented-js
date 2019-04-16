92b9e39bdebd224a8364896c6dd55d2886bde63e
abusayedrubel <abusayedrubel0@gmail.com>
2019-04-16 10:42:19 +0600

    js constructor practise

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-// Person Constructor
-function Person(name, age){
+//constructor
+function Person(name, dob){
   this.name = name;
-  this.birthday = new Date(age);
-
+  this.birthday = new Date(dob);
   this.calculateAge = function(){
-    const difference = Date.now()-this.birthday.getTime();
-    const getDate = new Date(difference);
-    return Math.abs(getDate.getUTCFullYear()-1970);
+    let difference = Date.now() - this.birthday.getTime();
+    let getAge = new Date(difference);
+    return Math.abs(getAge.getUTCFullYear()-1970);
   }
 }
 
-const value = new Person('rubel', '09-22-1995');
-
+const value = new Person('Sayed Rubel','09-22-1995');
 console.log(value.calculateAge());
